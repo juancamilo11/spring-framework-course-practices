39b74ecf20909c13e379c1e51cf70ee71a694b98
Juan Camilo Cardona Calderon <juan.cardona69@udea.edu.co>
2022-01-24 03:52:07 -0500

    Created pet-clinic practice

---
+++ pet-clinic-spring/src/test/java/dev/j3c/petclinic/PetClinicSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.j3c.petclinic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PetClinicSpringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
