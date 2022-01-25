1e2b0bdf16682249aa6caa526bf741b705080211
Juan Camilo Cardona Calderon <juan.cardona69@udea.edu.co>
2022-01-25 15:08:47 -0500

    Refactored interfaces with generics

--- pet-clinic-spring/pet-clinic-data/src/main/java/dev/j3c/services/OwnerService.java
+++ pet-clinic-spring/pet-clinic-data/src/main/java/dev/j3c/services/OwnerService.java
@@ -2,13 +2,6 @@ package dev.j3c.services;
 
 import dev.j3c.model.Owner;
 
-import java.util.Set;
-
-public interface OwnerService {
-
+public interface OwnerService extends CrudService<Owner, Long> {
     Owner findByLastName(String lastName);
-    Owner findById(Long id);
-    Owner save(Owner owner);
-    Set<Owner> findAll();
-
 }
--- pet-clinic-spring/pet-clinic-data/src/main/java/dev/j3c/services/PetService.java
+++ pet-clinic-spring/pet-clinic-data/src/main/java/dev/j3c/services/PetService.java
@@ -4,10 +4,6 @@ import dev.j3c.model.Pet;
 
 import java.util.Set;
 
-public interface PetService {
-
-    Pet findById(Long id);
-    Pet save(Pet pet);
-    Set<Pet> findAll();
+public interface PetService extends CrudService<Pet, Long> {
 
 }
