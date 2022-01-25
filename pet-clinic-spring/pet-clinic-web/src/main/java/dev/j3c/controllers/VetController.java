35fe297b38bba995b647bee8609c87c161f8510a
Juan Camilo Cardona Calderon <juan.cardona69@udea.edu.co>
2022-01-25 16:57:58 -0500

    Added controllers and view for owners and vets

---
+++ pet-clinic-spring/pet-clinic-web/src/main/java/dev/j3c/controllers/VetController.java
@@ -0,0 +1,14 @@
+package dev.j3c.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/vets")
+public class VetController {
+
+    @RequestMapping({"", "/", "/index", "/index.html"})
+    public String listVets() {
+        return "vets/index";
+    }
+}
